b324f774543614b51945ee3c80c49a01875a03ed
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2025-04-27 00:20:03 +0200

    Update SoundProcessorApplication.java

--- sound-processor/src/main/java/com/miskolc/sound_processor/SoundProcessorApplication.java
+++ sound-processor/src/main/java/com/miskolc/sound_processor/SoundProcessorApplication.java
@@ -2,10 +2,15 @@ package com.miskolc.sound_processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoundProcessorApplication {
 
+	@Bean
+	public ReceiveSoundLevel receiveSoundLevel() {
+		return new ReceiveSoundLevel();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SoundProcessorApplication.class, args);
 	}
