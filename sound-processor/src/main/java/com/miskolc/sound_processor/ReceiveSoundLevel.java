4fbc46d0de8de5228c44d074956dd5ff6130063b
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2025-04-27 00:19:57 +0200

    Create ReceiveSoundLevel.java

---
+++ sound-processor/src/main/java/com/miskolc/sound_processor/ReceiveSoundLevel.java
@@ -0,0 +1,61 @@
+package com.miskolc.sound_processor;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.TimeoutException;
+
+public class ReceiveSoundLevel {
+    private final String queueName = "soundLevelQueue";
+    private Channel channel;
+
+    private final Queue<Integer> queue = new LinkedList<>();
+    private final int maxSize = 5;
+    private long lastTic = System.currentTimeMillis();
+
+
+    public ReceiveSoundLevel() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.queueDeclare(queueName, false, false, false, null);
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+
+                if (queue.size() > maxSize) {
+                    queue.poll();
+                }
+
+                long currentTic = System.currentTimeMillis();
+                long timeDiff = currentTic - lastTic;
+
+                int soundLevel = Integer.parseInt(message);
+                if (soundLevel>80) {
+                    queue.add(soundLevel);
+                }
+                if (timeDiff > 30000) {
+                    System.out.println(" [x] Sound level: " + Collections.max(queue) + " dB");
+                    System.out.println(" [x] Time since last message: " + currentTic + " ms");
+                    lastTic = currentTic;
+                    queue.clear();
+                }
+            };
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
