8c1836dad7143cf67b360fa87a782a722cb966dd
Ibrahim Shaglil <ibrahimdevoloper@gmail.com>
2025-04-27 14:00:59 +0200

    added reciveing alerts in the 3rd component

--- alert-client/src/main/java/com/miskolc/alert_client/AlertClientApplication.java
+++ alert-client/src/main/java/com/miskolc/alert_client/AlertClientApplication.java
@@ -2,9 +2,15 @@ package com.miskolc.alert_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AlertClientApplication {
+
+	@Bean
+	public ReceiveSoundAlert receiveSoundAlert() {
+		return new ReceiveSoundAlert();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlertClientApplication.class, args);
---
+++ alert-client/src/main/java/com/miskolc/alert_client/ReceiveSoundAlert.java
@@ -0,0 +1,36 @@
+package com.miskolc.alert_client;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ReceiveSoundAlert {
+    private final String queueName = "soundAlertQueue";
+    private Channel channel;
+
+
+    public ReceiveSoundAlert() {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.queueDeclare(queueName, false, false, false, null);
+
+
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                String message = new String(delivery.getBody(), "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            };
+            channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
